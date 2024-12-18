047cb63d347fda801e17226562dfbfd311ae03a6
Dhanush4203 <166852919+Dhanush4203@users.noreply.github.com>
2024-12-18 15:33:43 +0530

    Add files via upload

---
+++ date.java
@@ -0,0 +1,13 @@
+package time_date;
+import java.time.LocalDate;
+
+//import java.lang.System;
+public class sample{
+public static void main(String args[]) {
+
+	
+	LocalDate d =  (LocalDate.now()).plusYears(4);
+	System.out.println(d);
+	
+}
+}
