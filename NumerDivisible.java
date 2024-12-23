57a3a8e1f6b95a24b2ff4b786f8b3bc792fe49a3
Dhanush4203 <166852919+Dhanush4203@users.noreply.github.com>
2024-12-23 21:47:12 +0530

    Add files via upload

---
+++ NumerDivisible.java
@@ -0,0 +1,21 @@
+package programtask;
+
+public class NumerDivisible {
+public static void main(String args[]) {
+		
+		int i=2;
+		while(i<=100) {
+			
+			if(i%2==0 && i%4==0) {
+				System.out.println(i);
+				
+			}
+			i++;
+			
+		}
+		
+		
+		
+		}
+
+}
